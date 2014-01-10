2610059b26e435408aa7a65204c673c4a118152c
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2014-01-10 15:43:44 +0900

    /cm/deployment dump
    Retrieve full description on the entire Cloudera Manager deployment including all hots, clusters, services, roles, users, settings, etc.
    This object can be used to reconstruct your entire deployment.

--- cmdump.py
+++ cmdump.py
@@ -4,21 +4,27 @@ __author__ = 'Kiyoshi Mizumaru'
 __email__ = 'maru@cloudera.com'
 
 import argparse
-import yaml
+import json
+import os
 import sys
+import yaml
 from cm_api.api_client import ApiResource
 
 CM_ACCESS = 'cm_access.yaml'
 
-parser = argparse.ArgumentParser(description='Cloudera Manager Dump')
-parser.add_argument('-c', '--conf', default=CM_ACCESS,
-                    help='Cloudera Manager access information (yaml)')
+parser = argparse.ArgumentParser(description='Cloudera Manager Deployment Dump')
+parser.add_argument('-c', '--conf', default=CM_ACCESS, metavar='yaml',
+                    help='cloudera manager access information')
+parser.add_argument('-o', '--output', default='', metavar='json',
+                    help='cloudera manager deployment output')
+parser.add_argument('-f', '--force', action='store_true',
+                    help='overwrite existing dump file')
 args = parser.parse_args()
 cm_access = None
 try:
     f = open(args.conf, 'r')
 except IOError as (errno, strerror):
-    print "IOerror: [Errno {0}] {1}: '{2}'".format(errno, strerror, args.conf)
+    print "IOError: [Errno {0}] {1}: '{2}'".format(errno, strerror, args.conf)
     sys.exit(-1)
 else:
     cm_access = yaml.load(f)
@@ -29,20 +35,22 @@ cm_port = cm_access['cm_port']
 username = cm_access['username']
 password = cm_access['password']
 
+if args.output:
+    output = args.output
+else:
+    output = '{0}:{1}.json'.format(cm_host, cm_port)
+
 api = ApiResource(cm_host, cm_port, username, password)
 
-cdh4 = None
-
-for c in api.get_all_clusters():
-    print c.name
-    if c.version == 'CDH4':
-        cdh4 = c
-
-for s in cdh4.get_all_services():
-    print s
-    if s.type == 'HDFS':
-        hdfs = s
-
-deployment = api.get('/cm/deployment')
-mgr = api.get_cloudera_manager()
-print mgr.__class__
+if not os.path.exists(output) or args.force:
+    try:
+        f = open(output, mode='w')
+    except IOError as (errno, strerror):
+        print "IOError: [Errno {0}] {1}: '{2}'".format(errno, strerror, output)
+        sys.exit(-1)
+    else:
+        json.dump(api.get('/cm/deployment'), f)
+        f.close()
+else:
+    print "output file already exists: '{0}'".format(output)
+    sys.exit(-1)
